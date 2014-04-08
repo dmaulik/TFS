df6868e1b8fad362add59aa940cc89820e6dcc46
levonnekey <lkey@usc.edu>
2014-04-07 19:06:19 -0700

    Update TFSChunkserver.java

--- src/TFS/TFSChunkserver.java
+++ src/TFS/TFSChunkserver.java
@@ -1,1 +1,35 @@
 package TFS;
+
+import java.io.File;
+import java.util.*;
+
+public class TFSChunkserver 
+{
+	protected String chunkLocation;
+	protected String [] chunkTable;
+	protected String root;
+	
+	public void write (int chunkuuid, File chunk)
+	{
+		String fileName;
+		File f = open (fileName, "read");
+	}
+	
+	private File open(String fileName, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String read (int chunkID)
+	{
+		String data = " ";
+		return data;
+	}
+	
+	public String getFileName (int chunkID)
+	{
+		
+		return root + "/" + Integer.toString(chunkID) + ".txt";
+	}
+
+}
