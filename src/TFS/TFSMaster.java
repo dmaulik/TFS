73b59c5b3ae51044ba5779a67accb8e1d93d6813
dmaulik <dmaulik93@gmail.com>
2014-04-07 18:20:48 -0700

    test commit, changes to tfsmaster

--- src/TFS/TFSMaster.java
+++ src/TFS/TFSMaster.java
@@ -1,4 +1,30 @@
 package TFS;
+
+import java.util.*;
+
 public class TFSMaster {
+    private int numChunks = 5;
+    private int MAX_CHUNKS = 10;
+    private int MAX_CHUNKS_PER_FILE = 100;
+    private int chunkSize = 10;
+    private int chunkRobin = 0;
+
+    private Map<Integer, TFSChunkserver> servers;
+
+    //self.filetable = {} TODO: change to HashMaps
+    //self.chunktable = {}
+
+    public TFSMaster() {
+        servers = new HashMap<Integer, TFSChunkserver>();
+
+        for(int i = 0; i < this.numChunks; i++){
+            TFSChunkserver cs = new TFSChunkserver(i);
+            servers.put(i, cs);
+        }
+    }
+
+    private Map getServers(){
+        return this.servers;
+    }
 	
 }
