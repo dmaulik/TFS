d2bcddea3742e04d73919536302b66848e8c1894
chengqu <chengqu@usc.edu>
2014-04-09 16:34:43 -0700

    jhjlk

--- src/TFS/TFSChunkserver.java
+++ src/TFS/TFSChunkserver.java
@@ -15,6 +15,7 @@ public class TFSChunkserver
 		this.chunkTable = new HashMap<Integer, String>();
 		this.root = "src";
 		this.local_filesystem_root = "/tmp/gfs/chunks" + chunkLocation.toString();
+		createFolder(this.local_filesystem_root);
 	}
 	
 	public void createFolder(String folderName){
