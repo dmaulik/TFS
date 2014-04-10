83ed6b522132cba52d9ae38fa89590dcb66c13d5
unknown <ewiraatm@dblab-03.vlab.usc.edu>
2014-04-10 11:26:51 -0700

    Test 5 done

--- src/TFS/Test5.java
+++ src/TFS/Test5.java
@@ -5,4 +5,35 @@ import java.io.*;
 
 public class Test5 {
 
+public static void storeTFSFile(TFSClient client, String filename, String lpath) throws IOException{
+		
+		if(!client.fileExists(filename)){
+			System.out.println("File doesn't exist in TFS");
+			return;
+		}
+		
+		File file = new File(lpath);
+		byte[] content = client.read(filename);
+		
+		FileOutputStream fos = new FileOutputStream(file);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		fos.write(content);
+		fos.flush();
+		fos.close();
+
+
+	} 
+	
+	public static void main (String[] args) throws IOException{
+		TFSMaster master = new TFSMaster();
+		TFSClient client = new TFSClient(master);
+		
+		String filename = "1\\2\\5\\File5";
+		String destination = "src\\test.csv";
+		String a = "QEQWEQWEQWEQWEQWEQWEWQE";
+		client.write(filename, a.getBytes());
+		storeTFSFile(client, filename, destination);
+	}
 }
