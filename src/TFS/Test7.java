751136d3df6435a2151fa3a4c117b24d9ddc754e
levonnekey <lkey@usc.edu>
2014-04-10 16:26:13 -0700

    update test7

--- src/TFS/Test7.java
+++ src/TFS/Test7.java
@@ -8,11 +8,17 @@ public class Test7 {
     public Test7() {}
 
     public static void countFile (TFSClient client, String filename) throws IOException {
-    	
+    	//If the input TFS file does not exist then return an error
     	if (!client.fileExists(filename))
     	{
     		System.out.println("Error: Input TFS file does not exists");
 		System.exit(0);
+    	}
+    	//count the number of logical files stored in a TFS file
+    	//read the size and payload pairs in the specified file name
+    	int numOfLogicalFiles;
+    	else {
+    		
     	}
     }
 
