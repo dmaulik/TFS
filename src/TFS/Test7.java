9cdd6f5cd98dd31155fb2cb9829f0d525c3572d5
levonnekey <lkey@usc.edu>
2014-04-10 15:58:23 -0700

    update test7

--- src/TFS/Test7.java
+++ src/TFS/Test7.java
@@ -5,6 +5,12 @@ import java.io.*;
 
 public class Test7 {
 
+    public Test7() {}
+
+    public static void countFile () throws IOException {
+    	
+    }
+
     public static void main (String[] args) throws Exception{
 		  TFSMaster master = new TFSMaster();
 	  	TFSClient client = new TFSClient(master);
