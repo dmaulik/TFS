a3b3b97fe7919f0e5169c8262eee9c375cfa70f4
levonnekey <lkey@usc.edu>
2014-04-18 14:07:53 -0700

    Update Unit1.java

--- src/TFS/Unit1.java
+++ src/TFS/Unit1.java
@@ -4,9 +4,21 @@ import java.io.*;
 import java.util.*;
 
 public class Unit1{
-
-
-  public static void main (String[] args) throws IOException{
+  
+  public static void makeDirs () {
     
   }
+
+  public static void makeFiles(String path, int n){
+		
+	}
+  public static void main (String[] args) throws IOException{
+      TFSMaster master = new TFSMaster();
+      TFSClient client = new TFSClient(master);
+      String pathname = "";
+      Scanner scan = new Scanner(System.in);
+     
+  }
+  
+  
 }
