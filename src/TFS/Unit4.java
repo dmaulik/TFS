a0be619e706116501eca29959298a31e763b3add
levonnekey <lkey@usc.edu>
2014-04-19 11:36:05 -0700

    Update Unit4.java

--- src/TFS/Unit4.java
+++ src/TFS/Unit4.java
@@ -4,9 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class Unit4{
-
+  public Unit4() {}
+  
 
   public static void main (String[] args) throws IOException{
+    TFSMaster master = new TFSMaster();
+		TFSClient client = new TFSClient(master);
     
+    String localpath = "";
+		String TFSfilePath = "";
+		
+		Scanner scan = new Scanner(System.in);
+		
   }
 }
