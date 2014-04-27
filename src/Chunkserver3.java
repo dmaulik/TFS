51d648604441365570048379409ab1caa5b00ef4
WAHAHAHAHA <ewiraatm@usc.edu>
2014-04-27 16:32:37 -0700

    asd

--- src/Chunkserver3.java
+++ src/Chunkserver3.java
@@ -1,7 +1,7 @@
 public class Chunkserver3{
 	
 	public static void main(String[] args){
-		new TFSChunkserver("3", 7503, "68.181.174.86");
+		new TFSChunkserver("3", 7503, "68.181.174.43");
 	}
 	
 }
