b067013f334c6d0d7bad36751c2754f9fb210969
WAHAHAHAHA <ewiraatm@usc.edu>
2014-04-28 12:19:28 -0700

    Asd

--- src/Main/Client/Client2.java
+++ src/Main/Client/Client2.java
@@ -4,7 +4,7 @@ public class Client2{
 	
 	public static void main(String[] args){
 		//params: Port#, your IP Address
-		new TFSClient(7498, "68.181.174.53");
+		new TFSClient(7498, "68.181.174.156");
 	}
 	
 }
